0d329ab4d5f9567d93fbbdc96ce4eb8e2a60d0db
Shashank5042 <130438579+Shashank5042@users.noreply.github.com>
2024-03-19 10:22:55 +0530

    Create feed forward neural networks.py

---
+++ feed forward neural networks.py
@@ -0,0 +1,13 @@
+Python 3.10.11 (tags/v3.10.11:7d4cc5a, Apr  5 2023, 00:38:17) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======== RESTART: C:/Users/ERINE DIYA/Desktop/python-ai/decision tree.py =======
+Enter the attributes to decide whether to buy a computer or not:
+Enter your budget (high/low): high
+Enter your credit status (good/bad): good
+Enter your age (young/middle-aged/senior): bad
+Are you a student? (yes/no): yes
+Decision: Buy
+>>> 
+======== RESTART: C:/Users/ERINE DIYA/Desktop/python-ai/decision tree.py =======
+Enter the attributes to decide whether to buy a computer or not:
